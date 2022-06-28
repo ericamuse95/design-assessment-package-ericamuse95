e3aa53bdad5b853437b0a683a9e2dca7ec9b04a9
Erica Muse <erica.muse@mykenzie.snhu.edu>
2022-06-27 23:50:10 -0800

    Design Assessment, local tests passed.

--- Application/src/main/java/com/kenzie/library/Application.java
+++ Application/src/main/java/com/kenzie/library/Application.java
@@ -3,6 +3,9 @@ package com.kenzie.library;
 //import dependencies
 import com.kenzie.library.book.service.BookRequestService;
 import com.kenzie.library.logger.service.Logger;
+import com.kenzie.library.model.LibraryBook;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
@@ -10,17 +13,34 @@ import java.util.ArrayList;
 public class Application {
 
     //TODO: Remove comments and complete method to convert a single response string to LibraryBook
-//    public static LibraryBook convertResponseToLibraryBook(String response){
-//
-//    }
+    public static LibraryBook convertResponseToLibraryBook(String response){
+        LibraryBook newBook = new LibraryBook();
+        newBook.setBookInfo(response);
+        return newBook;
+
+    }
 
     //TODO: Remove comments and complete method to return ArrayList of LibraryBook
-//    public static ArrayList<LibraryBook> returnBookList(String response1,
-//                                                        String response2,
-//                                                        String response3,
-//                                                        String response4,
-//                                                        String response5) {
-//    }
+    public static ArrayList<LibraryBook> returnBookList(String response1,
+                                                        String response2,
+                                                        String response3,
+                                                        String response4,
+                                                        String response5) {
+        ArrayList<LibraryBook> bookList = new ArrayList<>();
+        LibraryBook book1 = convertResponseToLibraryBook(response1);
+        LibraryBook book2 = convertResponseToLibraryBook(response2);
+        LibraryBook book3 = convertResponseToLibraryBook(response3);
+        LibraryBook book4 = convertResponseToLibraryBook(response4);
+        LibraryBook book5 = convertResponseToLibraryBook(response5);
+
+        bookList.add(book1);
+        bookList.add(book2);
+        bookList.add(book3);
+        bookList.add(book4);
+        bookList.add(book5);
+
+        return bookList;
+    }
 
     public static void main(String[] args) {
         //Use these constant values as input
@@ -33,10 +53,22 @@ public class Application {
         try {
             //Write code inside this try/catch block
             //1. Instantiate Singleton Logger object to log: "Process starting"
+            Logger logger = Logger.getInstance();
+            Logger.getInstance().logMessage("Process Starting");
             //2. Repeat for all five books listed (you do not need to loop):
             //      a. Call BookRequestService.sendGetRequest()
             //      b. Get the String response as the return value
             //      c. Use Logger to log each response string
+            BookRequestService.sendGetRequest(BOOK_1);
+            Logger.getInstance().logMessage(BOOK_1);
+            BookRequestService.sendGetRequest(BOOK_2);
+            Logger.getInstance().logMessage(BOOK_2);
+            BookRequestService.sendGetRequest(BOOK_3);
+            Logger.getInstance().logMessage(BOOK_3);
+            BookRequestService.sendGetRequest(BOOK_4);
+            Logger.getInstance().logMessage(BOOK_4);
+            BookRequestService.sendGetRequest(BOOK_5);
+            Logger.getInstance().logMessage(BOOK_5);
             // 3. Write a public static method convertResponseToLibraryBook() in Appplication.java
             //      a. the method takes in a response string
             //      b. instantiate a LibraryBook object
@@ -49,8 +81,11 @@ public class Application {
             //      d. return the Arraylist<LibraryBook>
             //5. Call the returnBookList() and print the results with System.out.println()
             //        to make sure the method works and toString() is overridden.
+            System.out.println(returnBookList(BOOK_1,BOOK_2,BOOK_3,BOOK_4,BOOK_5));
             //6. User Logger to log: "Process complete"
+            Logger.getInstance().logMessage("Process complete");
             //7. Unit Test your code with LibraryBookTest.java and ApplicationTest.java
+
 
     }
         catch (Exception e){
--- Application/src/main/java/com/kenzie/library/model/LibraryBook.java
+++ Application/src/main/java/com/kenzie/library/model/LibraryBook.java
@@ -1,6 +1,57 @@
 package com.kenzie.library.model;
 
 //TODO: Complete LibraryBook class by extending Book and adding required elements
-public class LibraryBook  {
 
+public class LibraryBook extends Book{
+
+    private String isbn = "";
+    private String subjectList = "";
+
+    //constructors
+    public LibraryBook(){
+        super();
+        isbn = getIsbn();
+        subjectList = getSubjectList();
+    }
+
+    public LibraryBook(String title, String author){
+        super(title, author);
+        this.title = title;
+        this.author = author;
+
+    }
+
+    //getter setter methods
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getSubjectList() {
+        return subjectList;
+    }
+
+    public void setSubjectList(String subjectList) {
+        this.subjectList = subjectList;
+    }
+    @Override
+    public String toString() {
+        return "LibraryBook{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", subjectList='" + subjectList + '\'' +
+                '}';
+    }
+    public void setBookInfo(String details){
+        String[] detailSplit = details.split("\\|");
+        setTitle(detailSplit[0]);
+        setAuthor(detailSplit[1]);
+        setIsbn(detailSplit[2]);
+        setSubjectList(detailSplit[3]);
+
+    }
 }
